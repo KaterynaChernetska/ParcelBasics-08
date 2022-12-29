8288914138f51e27bb8b6b138b6ab97ea66586d3
KaterynaChernetska <vesnavved@gmail.com>
2022-12-29 15:29:08 +0200

    Update js files

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,30 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+import localStorage from './localStorage';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const CURRENT_TIME_KEY = 'videoplayer-current-time';
+
+const onPlay = function (data) {
+
+    localStorage.save(CURRENT_TIME_KEY, data);  
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+player
+  .setCurrentTime(59.112)
+  .then(function (seconds) {
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        console.log('The time was less than 0 or greater than the video’s duration');
+        break;
+
+      default:
+
+        break;
+    }
+  });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,45 @@
+import localStorage from './localStorage';
+import throttle from 'lodash.throttle';
 
+
+const form = document.querySelector('.feedback-form');
+const VALUES_KEY = 'feedback-form-state';
+const formData = {};
+
+function getValue() {
+  const savedValues = localStorage.load(VALUES_KEY);
+
+  if (savedValues === undefined) {
+    return;
+  }
+
+  for (const elem in savedValues) {
+    form.elements[elem].value = savedValues[elem];
+  }
+}
+getValue();
+
+function onFormInput(event) {
+  const { target } = event;
+
+  const fieldValue = target.value;
+  const fieldName = target.name;
+
+  formData[fieldName] = fieldValue;
+
+  localStorage.save(VALUES_KEY, formData);
+}
+
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  console.log(formData);
+
+  localStorage.remove(VALUES_KEY);
+  form.reset(); 
+
+}
+
+form.addEventListener('input', throttle(onFormInput, 500));
+form.addEventListener('submit', onFormSubmit);
